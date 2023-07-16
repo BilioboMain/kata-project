0a6e3cefca630045d3f9217fe465f5bb7b4cf25a
gkrotov <Gleb.Krotov@noveogroup.com>
2023-07-16 22:16:01 +0300

    added test for cunjured item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -145,6 +145,14 @@ RSpec.describe GildedRose do
       expect(items[0].sell_in).to eq(-2)
     end
 
+    it "Brie doesn't lose quality and sell in after due date passing" do
+      items = [ConjuredItem.new('Conjured Item', 2, 8)]
+      GildedRose.new(items).update_quality
+      expect(items[0].name).to eq 'Conjured Item'
+      expect(items[0].quality).to eq 6
+      expect(items[0].sell_in).to eq 1
+    end
+
     it 'returns string with all attributes' do
       items = [AgedBrie.new('Aged Brie', -1, 50)]
       expect(items[0].to_s).to eq 'Aged Brie, -1, 50'
