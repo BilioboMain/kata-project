5dbce2e12796c1e4d5517c4b372ed3c42eb324b0
gkrotov <Gleb.Krotov@noveogroup.com>
2023-07-16 20:32:07 +0300

    added method for aged brie

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -5,27 +5,39 @@ class GildedRose
     @items = items
   end
 
+  def update_aged_brie(item)
+    item.sell_in -= 1
+
+    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.sell_in.negative? and item.quality < 50
+  end
+
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-      elsif item.quality < 50
-        item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
-          item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
-        end
-      end
-      item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
-      if item.sell_in.negative?
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-          else
-            item.quality = item.quality - item.quality
-          end
+      case item.name
+      when 'Aged Brie'
+        update_aged_brie(item)
+      else
+        if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+          item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
         elsif item.quality < 50
           item.quality = item.quality + 1
+          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+            item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
+            item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
+          end
+        end
+        item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+        if item.sell_in.negative?
+          if item.name != 'Aged Brie'
+            if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+              item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+            else
+              item.quality = item.quality - item.quality
+            end
+          elsif item.quality < 50
+            item.quality = item.quality + 1
+          end
         end
       end
     end
